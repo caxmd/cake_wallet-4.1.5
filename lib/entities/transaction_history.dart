dd290cfb21cd7bc7c07bc2edcc109bf46e452073
caxmd <lucavictor628@gmail.com>
2026-02-22 18:52:14 +0300

    Initial upload

---
+++ lib/entities/transaction_history.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+import 'package:cake_wallet/entities/transaction_info.dart';
+
+abstract class TransactionHistory {
+  Observable<List<TransactionInfo>> transactions;
+  Future<List<TransactionInfo>> getAll();
+  Future update();
+}
