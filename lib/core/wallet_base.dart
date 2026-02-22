6c3d39664ce036537ccacfb320c1052ec2277ac0
caxmd <lucavictor628@gmail.com>
2026-02-22 18:50:29 +0300

    Initial upload

---
+++ lib/core/wallet_base.dart
@@ -0,0 +1,59 @@
+import 'package:cake_wallet/entities/balance.dart';
+import 'package:cake_wallet/entities/transaction_priority.dart';
+import 'package:flutter/foundation.dart';
+import 'package:cake_wallet/entities/wallet_info.dart';
+import 'package:cake_wallet/core/pending_transaction.dart';
+import 'package:cake_wallet/core/transaction_history.dart';
+import 'package:cake_wallet/entities/currency_for_wallet_type.dart';
+import 'package:cake_wallet/entities/monero_transaction_priority.dart';
+import 'package:cake_wallet/entities/crypto_currency.dart';
+import 'package:cake_wallet/entities/sync_status.dart';
+import 'package:cake_wallet/entities/node.dart';
+import 'package:cake_wallet/entities/wallet_type.dart';
+
+abstract class WalletBase<BalanceType extends Balance> {
+  WalletBase(this.walletInfo);
+
+  static String idFor(String name, WalletType type) =>
+      walletTypeToString(type).toLowerCase() + '_' + name;
+
+  WalletInfo walletInfo;
+
+  WalletType get type => walletInfo.type;
+
+  CryptoCurrency get currency => currencyForWalletType(type);
+
+  String get id => walletInfo.id;
+
+  String get name => walletInfo.name;
+
+  String get address;
+
+  set address(String address);
+
+  BalanceType get balance;
+
+  SyncStatus get syncStatus;
+
+  set syncStatus(SyncStatus status);
+
+  String get seed;
+
+  Object get keys;
+
+  TransactionHistoryBase transactionHistory;
+
+  Future<void> connectToNode({@required Node node});
+
+  Future<void> startSync();
+
+  Future<PendingTransaction> createTransaction(Object credentials);
+
+  int calculateEstimatedFee(TransactionPriority priority, int amount);
+
+  Future<void> save();
+
+  Future<void> rescan({int height});
+
+  void close();
+}
