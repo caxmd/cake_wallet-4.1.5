63970c78c143766f24ed5755ccff20cc260e8f94
caxmd <lucavictor628@gmail.com>
2026-02-22 18:54:21 +0300

    Initial upload

---
+++ lib/reactions/on_current_node_change.dart
@@ -0,0 +1,16 @@
+import 'package:mobx/mobx.dart';
+import 'package:cake_wallet/entities/node.dart';
+import 'package:cake_wallet/store/app_store.dart';
+
+ReactionDisposer _onCurrentNodeChangeReaction;
+
+void startOnCurrentNodeChangeReaction(AppStore appStore) {
+  _onCurrentNodeChangeReaction?.reaction?.dispose();
+  appStore.settingsStore.nodes.observe((change) async {
+    try {
+      await appStore.wallet.connectToNode(node: change.newValue);
+    } catch (e) {
+      print(e.toString());
+    }
+  });
+}
