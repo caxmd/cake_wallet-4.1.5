f137c22a19feeab7b9243bcd4858a6b5392e173b
caxmd <lucavictor628@gmail.com>
2026-02-22 18:59:10 +0300

    Initial upload

---
+++ lib/src/widgets/cake_scrollbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CakeScrollbar extends StatelessWidget {
+  CakeScrollbar({
+    @required this.backgroundHeight,
+    @required this.thumbHeight,
+    @required this.fromTop
+  });
+
+  final double backgroundHeight;
+  final double thumbHeight;
+  final double fromTop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        right: 6,
+        child: Container(
+          height: backgroundHeight,
+          width: 6,
+          decoration: BoxDecoration(
+            color: Theme.of(context).textTheme.body1.decorationColor,
+            borderRadius: BorderRadius.all(Radius.circular(3))
+          ),
+          child: Stack(
+            children: <Widget>[
+              AnimatedPositioned(
+                duration: Duration(milliseconds: 0),
+                top: fromTop,
+                child: Container(
+                  height: thumbHeight,
+                  width: 6.0,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).textTheme.body1.color,
+                    borderRadius: BorderRadius.all(Radius.circular(3))
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
