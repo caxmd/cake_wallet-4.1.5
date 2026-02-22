518863ae625aa871bf06a8a77caf3b44b057b136
caxmd <lucavictor628@gmail.com>
2026-02-22 18:50:35 +0300

    Initial upload

---
+++ lib/core/wallet_credentials.dart
@@ -0,0 +1,10 @@
+import 'package:cake_wallet/entities/wallet_info.dart';
+
+abstract class WalletCredentials {
+  WalletCredentials({this.name, this.password, this.height, this.walletInfo});
+
+  final String name;
+  final int height;
+  String password;
+  WalletInfo walletInfo;
+}
